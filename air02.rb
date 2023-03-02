407e7b985eb4f6aec337a16125378a8b5a1258dc
paulprd <paulpaillard.23@gmail.com>
2023-03-02 18:40:54 +0100

    02

---
+++ air02.rb
@@ -0,0 +1,26 @@
+# Concat
+
+# Fonctions utilisées
+def array_to_str(to_concat, separator)
+	result = to_concat.join(separator)
+	return result
+end
+
+# Partie 1 : Gestion d'erreurs
+if ARGV.length < 3
+	puts "error: not enough arguments"
+	exit
+end
+
+# Partie 2 : Parsing
+separator = ARGV.last
+to_concat = ARGV
+to_concat.pop
+puts separator
+puts to_concat
+
+# Partie 3 : Résolution
+result = array_to_str(to_concat, separator)
+
+# Partie 4 : Affichage
+puts result
